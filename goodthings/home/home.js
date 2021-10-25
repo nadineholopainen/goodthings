64cba3c2672dd5bc4ed1fc595bcf2fc86a2947bd
Nadine Holopainen <92904095+nadineholopainen@users.noreply.github.com>
2021-10-26 09:28:44 +1300

    Add files via upload

---
+++ goodthings/home/home.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("#cf7_controls").on('click', 'span', function() {
+    $("#cf7 img").removeClass("opaque");
+
+    var newImage = $(this).index();
+
+    $("#cf7 img").eq(newImage).addClass("opaque");
+
+    $("#cf7_controls span").removeClass("selected");
+    $(this).addClass("selected");
+  });
+});
